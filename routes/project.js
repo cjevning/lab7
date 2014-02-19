db3eb545fe0fb6c0c6858808003355d43af8a285
vagrant <vagrant@precise32.(none)>
2014-02-19 22:51:15 +0000

    updated after calls

--- routes/project.js
+++ routes/project.js
@@ -21,7 +21,12 @@ exports.addProject = function(req, res) {
     "summary": form_data.summary,
     "image": form_data.image_url
   });
-  newProj.save(res.send());
+  newProj.save(afterSave);
+
+  function afterSave(err, projects) {
+    if(err) console.log(err);
+    res.send();
+  }
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
@@ -30,7 +35,12 @@ exports.addProject = function(req, res) {
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
 
-  models.Project.find({"_id": projectID}).remove().exec(res.send());
+  models.Project.find({"_id": projectID}).remove().exec(afterDelete);
+
+  function afterDelete(err, projects) {
+    if(err) console.log(err);
+    res.send()
+  }
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
